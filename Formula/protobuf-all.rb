4e92536d090cc458d429501a6347509a480092d0
liuys <liuyongsong@sunmi.com>
2021-05-26 11:46:42 +0800

    add package protobuf

---
+++ Formula/protobuf-all.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class ProtobufAll < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/liuysong/package/blob/b394f83d250d189d1e84bafd3a18950ce9e7835a/protobuf-all-3.6.1.tar.gz"
+  sha256 "22aec85e766dfc9facfa3b5ffe5999d2c05183fe6b40baa38bc207d3828144aa"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test protobuf-all`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
